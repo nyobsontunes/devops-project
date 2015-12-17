c3b14a2d9acbebf468d2c8fe9f1a0d4a9c2a312b
root <root@ubuntu-14.04-amd64-vbox>
2015-12-17 21:38:27 +0000

    chef-auto-reboot-ok

--- chef-repo/site-cookbooks/factorialapp/recipes/default.rb
+++ chef-repo/site-cookbooks/factorialapp/recipes/default.rb
@@ -14,3 +14,13 @@ package "python-pip"
 execute "install-python-module" do
 	command "pip install -r #{app_path}/requirements.txt"
 end
+
+if app_path != "/vagrant"
+	cookbook_file "/etc/init/factorial-api.conf" do
+		source "factorial-api.conf"
+	end
+	service "factorial-api" do
+		provider Chef::Provider::Service::Upstart
+	action [ :enable, :start ]
+	end
+end
